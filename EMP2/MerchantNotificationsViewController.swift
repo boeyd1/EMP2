4a73592ea9dfa36ae3144009a322f47bc7dc4df9
boeyd1 <dominic.boey@gmail.com>
2017-01-16 00:28:55 +0800

    created merchant notification vc

---
+++ EMP2/MerchantNotificationsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MerchantNotificationsViewController.swift
+//  EMP2
+//
+//  Created by Desmond Boey on 16/1/17.
+//  Copyright © 2017 DominicBoey. All rights reserved.
+//
+
+import UIKit
+
+class MerchantNotificationsViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    @IBAction func sendButtonTapped(_ sender: AnyObject) {
+    }
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
